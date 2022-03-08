62b54fb6a6e55b162744fc2e2d9637ed2b9da498
kolowy <paul.paul.dufour@gmail.com>
2022-03-08 12:31:42 +0100

    [ADD] camera movement + display PC

---
+++ Assets/Scripts/PCView/CameraMovement.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float zoomSpeed = 20.0f;
+    public float minZ = -40.0f;
+    public float maxZ = 40.0f;
+    public float minX = -40.0f;
+    public float maxX = 40.0f;
+    public float minY = 10.0f;
+    public float maxY = 50.0f;
+
+    public Text hourText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private string GetHour()
+    {
+        int hour = System.DateTime.Now.Hour;
+        string hourString = hour.ToString();
+        if (hour < 10)
+        {
+            hourString = "0" + hourString;
+        }
+
+        return "Wave number" + hourString;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x += speed * Time.deltaTime;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            y -= zoomSpeed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            y += zoomSpeed * Time.deltaTime;
+        }
+
+        x = Mathf.Clamp(x, minX, maxX);
+        y = Mathf.Clamp(y, minY, maxY);
+        z = Mathf.Clamp(z, minZ, maxZ);
+
+        transform.position = new Vector3(x, y, z);
+
+        hourText.text = GetHour();
+
+    }
+}
