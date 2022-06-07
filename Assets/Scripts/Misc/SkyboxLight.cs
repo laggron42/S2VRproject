c9b013bfc613e610e8f887bdaec64ff9a1a06440
daif <julie.fiadino@epita.fr>
2022-06-07 17:52:29 +0200

    Add Skycolors to script

--- Assets/Scripts/Misc/SkyboxLight.cs
+++ Assets/Scripts/Misc/SkyboxLight.cs
@@ -8,8 +8,36 @@ using UnityEngine;
 */
 public class SkyboxLight : MonoBehaviour
 {
+    public Renderer sky;
+    private Light sun;
+
+    [Header("Day Colors")]
+    public Color dayColor;
+    public Color dayHorizon;
+    public Color daySun;
+
+    [Header("Noon Colors")]
+    public Color noonColor;
+    public Color noonHorizon;
+    public Color noonSun;
+
+    [Header("Night Colors")]
+    public Color nightColor;
+    public Color nightHorizon;
+    public Color nightSun;
+
     void Start()
     {
         Shader.SetGlobalVector("_SunDirection", transform.forward);
+        sky.material.SetColor("_Sky_Color", dayColor);
+        sky.material.SetColor("_Horizon_Color", dayHorizon);
+        sky.material.SetColor("_Sun_Color", daySun);
+        sun = GetComponent<Light>();
+        sun.color = daySun;
+    }
+
+    void Update()
+    {
+        
     }
 }
