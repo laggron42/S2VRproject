ca6e99aa40d3c07b418d05aaa1c2e1e605f9924c
retke <laggron42@gmail.com>
2022-06-08 12:05:40 +0200

    Add a message when no headset is found

--- Assets/Scripts/LoadingScene/Loader.cs
+++ Assets/Scripts/LoadingScene/Loader.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Valve.VR;
 
 public class Loader : MonoBehaviour
 {
+    public GameObject buttons;
+    public GameObject error;
+    
     public static void LoadMenu()
     {
         LoadSceneData.nextSceneToLoad = "menu";
@@ -20,5 +25,13 @@ public class Loader : MonoBehaviour
     public static void Quit()
     {
         Application.Quit();
+    }
+
+    private void Start()
+    {
+        if (OpenVR.IsHmdPresent())
+            buttons.SetActive(true);
+        else
+            error.SetActive(true);
     }
 }
